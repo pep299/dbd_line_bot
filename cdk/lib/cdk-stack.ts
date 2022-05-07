f7959ac9b2ab2c265390c6c3d4c5831fd2094672
pep299 <eniarup@gmail.com>
2022-05-07 14:53:41 +0900

    fix: docker -> zip deploy

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,7 +1,7 @@
 import { Stack, StackProps, Duration } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { ManagedPolicy, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
-import { DockerImageFunction, DockerImageCode } from 'aws-cdk-lib/aws-lambda';
+import { Function, Code, Runtime,  } from 'aws-cdk-lib/aws-lambda';
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -15,8 +15,18 @@ export class CdkStack extends Stack {
       ],
     });
 
-    const batchStack = new DockerImageFunction(this, 'BatchFunction', {
-      code: DockerImageCode.fromImageAsset('../batch'),
+    const batchStack = new Function(this, 'BatchFunction', {
+      code: Code.fromAsset('../batch', {
+        bundling: {
+          image: Runtime.PYTHON_3_9.bundlingImage,
+          command: [
+            'bash', '-c',
+            'pip install -r requirements.txt -t /asset-output && cp -au . /asset-output',
+          ],
+        },
+      }),
+      runtime: Runtime.PYTHON_3_9,
+      handler: 'lambda_function.lambda_handler',
       environment: {
         TWITTER_ACCESS_TOKEN: '',
         TWITTER_ACCESS_TOKEN_SECRET: '',
