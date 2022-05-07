a69ee333c549508c9aa3c68d44fe3b3901abf38e
pep299 <eniarup@gmail.com>
2022-05-08 02:15:06 +0900

    add: iac webhook handler

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,7 +1,7 @@
 import { Stack, StackProps, Duration } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { ManagedPolicy, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
-import { Function, Code, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Function, Code, Runtime, FunctionUrlAuthType } from 'aws-cdk-lib/aws-lambda';
 import { RetentionDays } from 'aws-cdk-lib/aws-logs';
 import { Rule, Schedule } from 'aws-cdk-lib/aws-events';
 import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets';
@@ -19,6 +19,39 @@ export class CdkStack extends Stack {
       ],
     });
 
+    const lambdaEnv = {
+      LINE_CHANNEL_SECRET: process.env.LINE_CHANNEL_SECRET!!,
+      LINE_CHANNEL_ACCESS_TOKEN: process.env.LINE_CHANNEL_ACCESS_TOKEN!!,
+      S3_BUCKET_NAME: process.env.S3_BUCKET_NAME!!,
+      S3_KEY_NAME: process.env.S3_KEY_NAME!!,
+      TWITTER_ACCESS_TOKEN: process.env.TWITTER_ACCESS_TOKEN!!,
+      TWITTER_ACCESS_TOKEN_SECRET: process.env.TWITTER_ACCESS_TOKEN_SECRET!!,
+      TWITTER_CONSUMER_KEY: process.env.TWITTER_CONSUMER_KEY!!,
+      TWITTER_CONSUMER_SECRET: process.env.TWITTER_CONSUMER_SECRET!!,
+    };
+
+    const webhookHandlerStack = new Function(this, 'WebhookHandlerFunction', {
+      code: Code.fromAsset('../webhook_handler', {
+        bundling: {
+          image: Runtime.PYTHON_3_9.bundlingImage,
+          command: [
+            'bash', '-c',
+            'pip install -r requirements.txt -t /asset-output && cp -au . /asset-output',
+          ],
+        },
+      }),
+      runtime: Runtime.PYTHON_3_9,
+      handler: 'lambda_function.lambda_handler',
+      environment: lambdaEnv,
+      role: iamRoleForLambda,
+      timeout: Duration.minutes(5),
+      logRetention: RetentionDays.TWO_MONTHS,
+    });
+
+    const url = webhookHandlerStack.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+    });
+
     const batchStack = new Function(this, 'BatchFunction', {
       code: Code.fromAsset('../batch', {
         bundling: {
@@ -31,15 +64,7 @@ export class CdkStack extends Stack {
       }),
       runtime: Runtime.PYTHON_3_9,
       handler: 'lambda_function.lambda_handler',
-      environment: {
-        LINE_CHANNEL_ACCESS_TOKEN: process.env.LINE_CHANNEL_ACCESS_TOKEN!!,
-        S3_BUCKET_NAME: process.env.S3_BUCKET_NAME!!,
-        S3_KEY_NAME: process.env.S3_KEY_NAME!!,
-        TWITTER_ACCESS_TOKEN: process.env.TWITTER_ACCESS_TOKEN!!,
-        TWITTER_ACCESS_TOKEN_SECRET: process.env.TWITTER_ACCESS_TOKEN_SECRET!!,
-        TWITTER_CONSUMER_KEY: process.env.TWITTER_CONSUMER_KEY!!,
-        TWITTER_CONSUMER_SECRET: process.env.TWITTER_CONSUMER_SECRET!!,
-      },
+      environment: lambdaEnv,
       role: iamRoleForLambda,
       timeout: Duration.minutes(5),
       logRetention: RetentionDays.TWO_MONTHS,
